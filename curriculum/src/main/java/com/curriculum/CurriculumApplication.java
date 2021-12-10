99a758f795edecf6a494a4860c0ef65f2cb63135
“gil-son” <gilszon@gmail.com>
2021-12-10 19:07:22 -0300

    Initial

---
+++ curriculum/src/main/java/com/curriculum/CurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumApplication.class, args);
+	}
+
+}
---
+++ curriculum/src/test/java/com/curriculum/CurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
