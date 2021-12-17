e8816d483102f98d46d7348494a0e9149bd15348
“gil-son” <gilszon@gmail.com>
2021-12-17 19:52:43 -0300

    Data - SQL

---
+++ curriculum/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_subject (name, description) VALUES ('Art','Description Art');
+INSERT INTO tb_subject (name, description) VALUES ('Description English');
+INSERT INTO tb_subject (name, description) VALUES ('Description Science');
+
+INSERT INTO tb_student (name, age) VALUES ('Alex', 19');
+INSERT INTO tb_student (name, age) VALUES ('Cloud', 21');
+INSERT INTO tb_student (name, age) VALUES ('Leona', 20,);
+INSERT INTO tb_student (name, age) VALUES ('Russe', 19,);
+
+INSERT INTO tb_student_subject (student_id, subject_id) VALUES (1, 2);
+INSERT INTO tb_student_subject (student_id, subject_id) VALUES (2, 1);
+INSERT INTO tb_student_subject (student_id, subject_id) VALUES (2, 3);
+INSERT INTO tb_student_subject (student_id, subject_id) VALUES (3, 3);
+
