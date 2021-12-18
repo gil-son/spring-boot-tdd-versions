fc6ae20a8fe00c76e4ad5d70fa65970164119a48
“gil-son” <gilszon@gmail.com>
2021-12-17 21:19:08 -0300

    Films

---
+++ films/src/main/java/com/films/FilmsApplication.java
@@ -0,0 +1,13 @@
+package com.films;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsApplication.class, args);
+	}
+
+}
---
+++ films/src/test/java/com/films/FilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
